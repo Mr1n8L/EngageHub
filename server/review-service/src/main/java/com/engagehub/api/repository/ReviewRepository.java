50ec2e1dc88225700c5cfd69475347eec71f59aa
Mr1n8L <mrinaljani1822@gmail.com>
2024-08-19 10:01:08 +0530

    microservices config

--- server/review-service/src/main/java/com/engagehub/api/repository/ReviewRepository.java
+++ server/review-service/src/main/java/com/engagehub/api/repository/ReviewRepository.java
@@ -4,7 +4,9 @@ import com.engagehub.api.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    // Custom query methods can be added here if needed
+    List<Review> findByRating(int rating);
 }
