52db8b619941b5d7d1e63146fc0352408ea463cc
Mr1n8L <mrinaljani1822@gmail.com>
2024-09-05 20:59:14 +0530

    service discovery config

--- server/service-discovery/src/main/java/com/engagehub/api/ServiceDiscoveryApplication.java
+++ server/service-discovery/src/main/java/com/engagehub/api/ServiceDiscoveryApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// Enable Eureka Server to act as a service registry and discovery server
 @EnableEurekaServer
+
 public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
