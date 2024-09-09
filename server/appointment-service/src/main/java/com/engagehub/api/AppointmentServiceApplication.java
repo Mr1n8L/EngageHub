8ab1ad5591647a3ee2d2055c28796fd371bf44fb
Mr1n8L <mrinaljani1822@gmail.com>
2024-09-09 23:06:04 +0530

    service discovery config

--- server/appointment-service/src/main/java/com/engagehub/api/AppointmentServiceApplication.java
+++ server/appointment-service/src/main/java/com/engagehub/api/AppointmentServiceApplication.java
@@ -2,12 +2,13 @@ package com.engagehub.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  // Enabling Eureka Client
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentServiceApplication.class, args);
 	}
-
 }
