2bda3737e3215e0733a10fb8184f657dc95f147a
Mrinal <158752990+Mr1n8L@users.noreply.github.com>
2024-09-20 23:43:56 +0530

    Update BusinessRepository.java

--- server/business-service/src/main/java/com/engagehub/api/repository/BusinessRepository.java
+++ server/business-service/src/main/java/com/engagehub/api/repository/BusinessRepository.java
@@ -1,9 +1,8 @@
 package com.engagehub.api.repository;
-
 import com.engagehub.api.model.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//extending JPA Repository
 @Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
 }
