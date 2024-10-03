29ae0975d195ecb8945f8b4fb744a2624caa8bce
Mr1n8L <mrinaljani1822@gmail.com>
2024-10-03 10:09:59 +0530

    removing Spring Cloud - Eureka Client config

--- server/customer-service/src/main/java/com/engagehub/api/CustomerServiceApplication.java
+++ server/customer-service/src/main/java/com/engagehub/api/CustomerServiceApplication.java
@@ -2,10 +2,10 @@ package com.engagehub.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
